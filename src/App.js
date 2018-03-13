d3e7a371c5a6ce7a3f044020bc261b4d09d56c02
Zach <zachschatz@gmail.com>
2018-03-13 14:59:24 -0400

    Remove violation of p tag holding a div

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Calculator from './calculator.js';
+import Calculator from'./calculator.js';
 
 class App extends Component {
   render() {
@@ -11,9 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
+        <div className="App-intro">
           <Calculator />
-        </p>
+        </div>
       </div>
     );
   }
